df17838fdac03c0f4176fd22b72b14e8994b1b69
Nick Wang <nickyiliwang@gmail.com>
2019-11-28 10:47:54 -0500

    Updates

---
+++ precache-manifest.590afdccc02a2c0a521e2fbd3b675823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc51f9605f7eb0595f18c8dda5b9fa4",
+    "url": "/react-basics/index.html"
+  },
+  {
+    "revision": "f0fa79790784f008bdec",
+    "url": "/react-basics/static/css/main.26368e65.chunk.css"
+  },
+  {
+    "revision": "64ddddf0121818386475",
+    "url": "/react-basics/static/js/2.3c4c2211.chunk.js"
+  },
+  {
+    "revision": "f0fa79790784f008bdec",
+    "url": "/react-basics/static/js/main.d80cce63.chunk.js"
+  },
+  {
+    "revision": "18c63a5f0c4d42252292",
+    "url": "/react-basics/static/js/runtime-main.fa1840e6.js"
+  }
+]);
